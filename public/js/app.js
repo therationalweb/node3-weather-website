88b857884b252c32daf24d51cb7f2f9cdac034e8
Tom Bishop <therationalweb@gmail.com>
2020-10-29 13:54:32 -0400

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-console.log('Client side javascript is loaded.')
-
-// fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-//   response.json().then((data) => {
-//     if(data.error){
-//       console.log(data.error)
-//     }
-//     else{
-//       console.log(data.location)
-//       console.log(data.forecast)
-//     }
-//   })
-// })
-
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const message1 = document.querySelector('#messageOne')
